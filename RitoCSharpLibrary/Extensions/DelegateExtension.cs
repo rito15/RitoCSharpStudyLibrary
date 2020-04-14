2c3a5227eeeca5c2133dd0170d534281a1836541
rito <ghdtkals15@naver.com>
2020-04-14 17:17:55 +0900

    델리게이트 메소드, 확장 메소드

---
+++ RitoCSharpLibrary/Extensions/DelegateExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rito
+{
+    // 작성 : 2020. 04. 14
+    public static class DelegateExtension
+    {
+        /// <summary> 델리게이트 또는 이벤트 변수가 해당 메소드를 갖고 있는지 여부 검사 </summary>
+        public static bool Ex_Contains<T>(this T @delegate, in T method) where T : Delegate
+        {
+            if (@delegate == null || method == null) return false;
+            return @delegate.GetInvocationList().Contains(method);
+        }
+
+        /// <summary> 델리게이트 또는 이벤트 변수가 갖고 있는 메소드 개수 </summary>
+        public static int Ex_MethodCount<T>(this T @delegate) where T : Delegate
+        {
+            if (@delegate == null) return 0;
+            return @delegate.GetInvocationList().Length;
+        }
+
+        /// <summary> 델리게이트 또는 이벤트 변수가 해당 메소드를 몇 개나 갖고 있는지 검사하여 리턴 </summary>
+        public static int Ex_CheckSpecificMethodCount<T>(this T @delegate, in T method) where T : Delegate
+        {
+            if (@delegate == null || method == null) return 0;
+
+            int count = 0;
+            var methods = @delegate.GetInvocationList();
+            for (int i = 0; i < methods.Length; i++)
+            {
+                if (methods[i] == method)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- RitoCSharpLibrary/Rito/RitoDelegate.cs
+++ RitoCSharpLibrary/Rito/RitoDelegate.cs
@@ -17,5 +17,12 @@ namespace Rito
         //public delegate void sendThreeParamType<T, U, V>(T param1, U param2, V param3);
 
         public delegate void sendOneRefParamType<T>(ref T param);
+
+        /// <summary> 델리게이트 또는 이벤트 변수가 해당 메소드를 갖고 있는지 여부 확인 </summary>
+        public static bool CheckContained<T>(in T @delegate, in T method) where T : Delegate
+        {
+            if (@delegate == null || method == null) return false;
+            return @delegate.GetInvocationList().Contains(method);
+        }
     }
 }
